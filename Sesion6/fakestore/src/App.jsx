0e06150cdd205e6f550a5b34d131693baf40e80c
SamuelJJuarez <148638362+SamuelJJuarez@users.noreply.github.com>
2023-11-18 12:55:10 -0600

    Acabado

--- Sesion6/fakestore/src/App.jsx
+++ Sesion6/fakestore/src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
-    <BrowserRouter>
-      <main className="min-h-screen min-w-screen bg-gray-100 flex flex-col">
-        <Navbar />
-        <div className="w-full flex justify-center">
-          <div className="container flex text-white p-4">
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
+    <Provider store={store}>
+      <BrowserRouter>
+        <main className="min-h-screen min-w-screen bg-gray-100 flex flex-col">
+          <Navbar />
+          <div className="w-full flex justify-center">
+            <div className="container flex text-white p-4">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path ="/cart" element={<Cart />}></Route>
+              </Routes>
+            </div>
           </div>
-        </div>
-      </main>
-    </BrowserRouter>
+        </main>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
