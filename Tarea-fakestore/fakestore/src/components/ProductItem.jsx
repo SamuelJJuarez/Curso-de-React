39666a132d2e362cc468140e3f44f22afea46d8c
SamuelJJuarez <148638362+SamuelJJuarez@users.noreply.github.com>
2023-12-02 13:51:13 -0600

    Update ProductItem.jsx

--- Tarea-fakestore/fakestore/src/components/ProductItem.jsx
+++ Tarea-fakestore/fakestore/src/components/ProductItem.jsx
@@ -26,17 +26,17 @@ const ProductItem = ({product}) => {
         <img src={product.image} className="w-20" />
         <span className="text-center font-bold text-black">{product.title}</span>
         <span className="text-center font-bold text-sm text-black">${product.price}</span>
-        if(validation === true){
+        {validation === true ? (
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={addToCart}>
                 Añadir al carrito
             </button>
-        } else {
+        ) : (
             <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
             onClick={removeOfCart}>
                 Eliminar del carrito
             </button>
-        }
+        )}
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             <Link to="/info">Más información</Link>
         </button>
